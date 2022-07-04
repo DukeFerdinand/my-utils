f3a9fbd2368e170a18d235d6da8eec8e494bcf77
Doug Flynn <DouglassHooker@gmail.com>
2022-07-04 23:36:01 +0000

    Add tests for Err variant of result class

--- packages/results/tests/index.spec.ts
+++ packages/results/tests/index.spec.ts
@@ -1,44 +1,44 @@
-import { ok, err, ResultType } from "../src/index";
+import { ok, err, ResultType } from '../src/index';
 
-describe("Tyescript Result classes", () => {
-  const testCases = {
-    ok: ok("ok-val"),
-    err: err("err-val"),
-  };
+describe('Tyescript Result classes', () => {
+    const testCases = {
+        ok: ok('ok-val'),
+        err: err('err-val'),
+    };
 
-  describe("# Ok Instance", () => {
-    it('has an "Ok" type', () => {
-      expect(testCases.ok.type).toBe(ResultType.Ok);
+    describe('# Ok Instance', () => {
+        it('has an "Ok" type', () => {
+            expect(testCases.ok.type).toBe(ResultType.Ok);
+        });
+
+        describe('getVal', () => {
+            it('returns wrapped value', () => {
+                expect(testCases.ok.getVal()).toBe('ok-val');
+            });
+        });
+
+        describe('getErr', () => {
+            it('throws an error', () => {
+                expect(() => testCases.ok.getErr()).toThrow();
+            });
+        });
     });
 
-    describe("getVal", () => {
-      it("returns wrapped value", () => {
-        expect(testCases.ok.getVal()).toBe("ok-val");
-      });
-    });
+    describe('# Err Instance', () => {
+        it('has an "Err" type', () => {
+            expect(testCases.err.type).toBe(ResultType.Err);
+        });
 
-    describe("getErr", () => {
-      it("throws an error", () => {
-        expect(() => testCases.ok.getErr()).toThrow();
-      });
-    });
-  });
+        describe('getVal', () => {
+            it('throws an error', () => {
+                expect(() => testCases.err.getVal()).toThrow();
+            });
+        });
 
-  describe("# Ok Instance", () => {
-    it('has an "Ok" type', () => {
-      expect(testCases.ok.type).toBe(ResultType.Ok);
+        describe('getErr', () => {
+            it('returns wrapped err value', () => {
+                expect(testCases.err.getErr()).toBe('err-val');
+            });
+        });
     });
-
-    describe("getVal", () => {
-      it("returns wrapped value", () => {
-        expect(testCases.ok.getVal()).toBe("ok-val");
-      });
-    });
-
-    describe("getErr", () => {
-      it("throws an error", () => {
-        expect(() => testCases.ok.getErr()).toThrow();
-      });
-    });
-  });
 });
